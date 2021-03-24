d855e3cd8afec02e93c5aff093db4ce6c139e90b
JEANS <JEANS@PAOLO>
2021-03-24 12:02:55 +0100

    Subiendo rama

--- src/Saludo.java
+++ src/Saludo.java
@@ -4,13 +4,40 @@ public class Saludo {
 	
 	public static void main(String arg[]) {
 		
-		String linea = "";
+		/*String linea = "";
 		
 		Scanner ent = new Scanner(System.in);
 		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
-		System.out.println("Introduce un número para averiguar si es primo o no:");
 		linea = ent.next();
-		System.out.println("Hola " + linea);
+		System.out.println("Hola " + linea);*/
+		
+		System.out.println("Hola");
+		System.out.println("Introduce un número para averiguar si es primo o no:");
+		int contador=0;;
+		int I=0;
+		int numero =0;
+		Scanner ent = new Scanner(System.in);
+        
+ 
+        System.out.print("Ingresa un numero: ");
+        numero = ent.nextInt();
+ 
+        contador = 0;
+ 
+        for(I = 1; I <= numero; I++)
+        {
+            if((numero % I) == 0)
+            {
+                contador++;
+            }
+        }
+ 
+        if(contador <= 2)
+        {
+            System.out.println("El numero es primo");
+        }else{
+            System.out.println("El numero no es primo");
+        }
 	}
 
 }
